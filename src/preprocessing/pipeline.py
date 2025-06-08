ba0975db30063e259af430094222ba6af07d9ca8
Suttawee Thongkao <112872479+PomSuttawee@users.noreply.github.com>
2025-06-08 15:47:58 +0700

    Fix import and usage of trim_reference_lines in full_preprocess function

--- src/preprocessing/pipeline.py
+++ src/preprocessing/pipeline.py
@@ -1,8 +1,8 @@
 from .steps.crop_plate import crop_plate
-from .steps.trim_ref_lines import trim_ref_lines
+from .steps.trim_ref_lines import trim_reference_lines
 from .steps.segment_spots import segment_spots
 
 def full_preprocess(img):
     img = crop_plate(img)
-    img = trim_ref_lines(img)
+    img = trim_reference_lines(img)
     return segment_spots(img)
