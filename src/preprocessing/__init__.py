0d27b88f7af12c372aa26b5fca4e7ccd99e51dc1
Suttawee Thongkao <112872479+PomSuttawee@users.noreply.github.com>
2025-06-08 15:47:03 +0700

    Update __init__.py: Include trim_reference_lines in the preprocessing module exports

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,5 +1,6 @@
 from .pipeline import full_preprocess
 from .steps.crop_plate import crop_plate
-from .steps.segment_spots import segment_spots  # plural reads nicely
+from .steps.trim_ref_lines import trim_reference_lines
+from .steps.segment_spots import segment_spots
 
-__all__ = ["full_preprocess", "crop_plate", "segment_spots"]
+__all__ = ["full_preprocess", "trim_reference_lines", "crop_plate", "segment_spots"]
